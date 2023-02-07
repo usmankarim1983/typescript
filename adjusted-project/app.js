88e0f38003136d6e4d9cad278076379eba0a3b0d
Osman Karim <usmankarim2022@gmail.com>
2023-02-07 17:12:13 -0500

    first commit

---
+++ adjusted-project/app.js
@@ -0,0 +1,12 @@
+// Core data types
+// string, number, boolean, any, void
+//How to user variable 
+var firstName = 'Osman';
+var num = 2.3;
+var dec = false;
+// Code example
+function add(num1, num2) {
+    return num1 + num2;
+}
+var result = add(5, 10);
+console.warn(result);
---
+++ adjusted-project/app.ts
@@ -0,0 +1,17 @@
+// Core data types
+// string, number, boolean, any, void
+
+//How to user variable 
+let firstName : string = 'Osman';
+let num : number = 2.3;
+let dec : boolean = false
+
+
+
+// Code example
+
+function add(num1, num2){
+    return num1 + num2
+}
+let result = add(5,10)
+console.log(result)
